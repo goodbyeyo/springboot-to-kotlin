a0f39c2a36bfd348ea17dbcfaf38274e5590bb19
Hwang Sangwook <goodbyeyo@naver.com>
2024-10-18 14:21:30 +0900

    change to kotlin in controller layer

---
+++ src/main/kotlin/com/group/libraryapp/controller/user/UserController.kt
@@ -0,0 +1,34 @@
+package com.group.libraryapp.controller.user
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import com.group.libraryapp.service.user.UserService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class UserController (
+    private val userService: UserService
+) {
+
+    @PostMapping("/user")
+    fun saveUser(@RequestBody request: UserCreateRequest) {
+        return userService.saveUser(request)
+    }
+
+    @GetMapping("/user")
+    fun getUsers(): List<UserResponse> = userService.findUsers()
+//    fun getUsers(): List<UserResponse> {
+//        return userService.findUsers()
+//    }
+
+    @PutMapping("/user")
+    fun updateUserName(@RequestBody request: UserUpdateRequest) {
+        userService.updateUserName(request)
+    }
+
+    @DeleteMapping("/user")
+    fun deleteUser(@RequestBody name: String) {
+        userService.deleteUser(name)
+    }
+}
